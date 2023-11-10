1ab5b92799da4c24c429c781b9b6c01a4253b9fa
91799 <91799@Sivaprasad>
2023-11-10 14:08:35 +0530

    3-commit=10/11/2023

--- Collection/src/com/methods/Collection_Methods.java
+++ Collection/src/com/methods/Collection_Methods.java
@@ -10,6 +10,7 @@ public class Collection_Methods {
 
 	public static void main(String[] args) {
 		System.out.println("main");
+		System.out.println("main");
 		//isEmpty();
 		//size();
 		//add();//addAll();
